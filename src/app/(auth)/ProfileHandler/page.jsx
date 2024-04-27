5caa86a5d58997692e28ed65aed540cdb263f908
Leo Gavin <leogavin123@outlook.com>
2024-04-27 22:42:35 +0530

    Update page.jsx

--- src/app/(auth)/ProfileHandler/page.jsx
+++ src/app/(auth)/ProfileHandler/page.jsx
@@ -2,7 +2,7 @@
 import { useEffect, useState } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
 import axios from 'axios';
-import Loader from '../../../components/Loader';
+import { Loader } from '../../components/Loader'
 
 export default function ProfileHandler() {
   const router = useRouter();
